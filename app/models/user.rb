538b17587b329b7733dbf1a83f451072c545303f
Kenneth Barrett <hamptonalumkb@protonmail.com>
2019-02-24 02:43:38 +0000

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   
+  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+  
+  validates :username, presence: true, 
+            uniqueness: { case_sensitive: false }, 
+            length: { minimum: 3, maximum: 25 }
+  
+  validates :email, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 },
+            format: { with: EMAIL_REGEX }
 end
